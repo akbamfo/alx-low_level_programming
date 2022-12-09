b6071ea505131bdadcd99e00a5f11bc3c6af2a79
Kofi Bamfo <bamfoak@gmail.com>
2022-12-08 17:44:03 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - print a C program followed by a new line tp the standard error
  * Return: Always 1
  */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
