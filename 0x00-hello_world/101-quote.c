04fc74ec5b01b7788e95c30866a62c2b00a34285
Kofi Bamfo <bamfoak@gmail.com>
2022-12-08 17:13:07 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print a C program that pice of art followed by a new line tp the standard error
- *Return: 1
+ * main - print a C program followed by a new line tp the standard error
+ *Return: always 1
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
