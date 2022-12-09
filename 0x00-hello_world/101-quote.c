83d2f61f18ea96f5a526531336fda425b3b79225
Kofi Bamfo <bamfoak@gmail.com>
2022-12-08 18:18:19 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - print a C program followed by a new line tp the standard error
  * Return:1
+ * *
  */
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
 	return (1);
 }
