1e433462c1a341ab8a03664ff45576d0152f5e17
Kofi Bamfo <bamfoak@gmail.com>
2022-12-08 19:11:59 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 
 /**
  * main - print a C program
- * Return: Always 1
- * *
- **/
+ * Return: 1 
+ */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
