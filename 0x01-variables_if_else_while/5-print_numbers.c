5e207af661f89ac6ad87d8f6459befe9f38e8802
Kofi Bamfo <bamfoak@gmail.com>
2022-12-11 13:53:44 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - assign a num. to the var n each time it is executed
+ * print the last digit of the num. stored in the var n.
+ *  Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
