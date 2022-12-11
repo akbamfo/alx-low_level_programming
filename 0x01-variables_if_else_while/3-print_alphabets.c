7e9448fd5b795dbb322f7c2ac6206ec0fa9904aa
Kofi Bamfo <bamfoak@gmail.com>
2022-12-11 13:17:45 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase and in uppercase
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
