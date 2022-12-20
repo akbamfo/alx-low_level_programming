86e17e1615772dffd856741e2af168a59f17f586
Kofi Bamfo <bamfoak@gmail.com>
2022-12-20 14:26:06 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+@@ -0, 0 + 1, 16 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: return the length of a string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
